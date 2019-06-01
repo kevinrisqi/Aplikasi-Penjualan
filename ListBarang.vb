1466981e64cc39122fa73cac7269657a5f37822f
Kevin <kevinrisqi22@gmail.com>
2019-06-01 19:57:49 +0700

    Make Daftar Barang in Crystal Report

--- ListBarang.vb
+++ ListBarang.vb
@@ -1,3 +1,28 @@
-﻿Public Class ListBarang
+﻿Imports CrystalDecisions.CrystalReports.Engine
+Imports System.Data.Odbc
+Imports CrystalDecisions.Shared
+Imports System.Data.SqlClient
+Imports System.Data
+Public Class ListBarang
 
+    Private Sub ListBarang_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Call listBarang()
+    End Sub
+
+    Sub listBarang()
+        Call koneksi()
+        Dim strPath As String
+        strPath = Application.StartupPath
+        strPath = strPath.Substring(0, strPath.LastIndexOf("\"))
+        strPath = strPath.Substring(0, strPath.LastIndexOf("\"))
+        strPath = strPath + "\"
+        Dim rpt As New ReportDocument
+        Da = New OdbcDataAdapter("select id_barang from barang", Conn)
+        Ds = New DataSet
+        Da.Fill(Ds, "barang")
+        rpt.Load(strPath + "\Reports\DaftarBarang.rpt")
+        rpt.SetDataSource(Ds.Tables(0))
+        CrystalReportViewer1.ReportSource = rpt
+        CrystalReportViewer1.Refresh()
+    End Sub
 End Class
