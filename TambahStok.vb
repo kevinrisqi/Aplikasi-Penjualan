3f7fdbfe90476049d57ebe4ff6bc7f5d67f56112
Kevin <kevinrisqi22@gmail.com>
2019-07-05 17:15:01 +0700

    Make TambahStok

---
+++ TambahStok.vb
@@ -0,0 +1,6 @@
+﻿Public Class TambahStok
+
+    Private Sub BunifuCustomDataGrid2_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles BunifuCustomDataGrid2.CellContentClick
+
+    End Sub
+End Class
